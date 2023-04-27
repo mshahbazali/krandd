03f7f7b5b7accd767248c6dd6f16d9b7ca49956c
Shahbaz Ali <mshahbazali563@gmail.com>
2023-04-27 23:41:37 +0500

    fixed width issue

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import JsPDF from "jspdf";
 
 const PDFButton = () => {
   const generatePDF = () => {
-    const report = new JsPDF("portrait", "pt", "a4");
+    const report = new JsPDF({ putOnlyUsedFonts: true, orientation: "landscape" });
     report.html(document.querySelector("#report")).then(() => {
       report.save("report.pdf");
     });
@@ -11,43 +11,47 @@ const PDFButton = () => {
 
   return (
     <>
-      <div id="report">
-        <table>
-          <tr>
-            <th>Company</th>
-            <th>Contact</th>
-            <th>Country</th>
-          </tr>
-          <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
-          </tr>
-          <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
-          </tr>
-          <tr>
-            <td>Ernst Handel</td>
-            <td>Roland Mendel</td>
-            <td>Austria</td>
-          </tr>
-          <tr>
-            <td>Island Trading</td>
-            <td>Helen Bennett</td>
-            <td>UK</td>
-          </tr>
-          <tr>
-            <td>Laughing Bacchus Winecellars</td>
-            <td>Yoshi Tannamuri</td>
-            <td>Canada</td>
-          </tr>
-          <tr>
-            <td>Magazzini Alimentari Riuniti</td>
-            <td>Giovanni Rovelli</td>
-            <td>Italy</td>
-          </tr>
+      <div>
+        <table style={{ width: "200px", tableLayout: "fixed" }} id="report">
+          <thead>
+            <tr>
+              <th style={{ width: "10%" , fontSize: '10px'}}>Company</th>
+              <th style={{ width: "10%" , fontSize: '10px'}}>Contact</th>
+              <th style={{ width: "10%" , fontSize: '10px'}}>Country</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td style={{ width: "10%" , fontSize: '10px'}}>Alfreds Futterkiste</td>
+              <td style={{ width: "10%" , fontSize: '10px'}}>Maria Anders</td>
+              <td style={{ width: "10%" , fontSize: '10px'}}>Germany</td>
+            </tr>
+            <tr>
+              <td style={{ width: "10%" , fontSize: '10px'}}>Centro comercial Moctezuma</td>
+              <td style={{ width: "10%" , fontSize: '10px'}}>Francisco Chang</td>
+              <td style={{ width: "10%" , fontSize: '10px'}}>Mexico</td>
+            </tr>
+            <tr>
+              <td style={{ width: "10%" , fontSize: '10px'}}>Ernst Handel</td>
+              <td style={{ width: "10%" , fontSize: '10px'}}>Roland Mendel</td>
+              <td style={{ width: "10%" , fontSize: '10px'}}>Austria</td>
+            </tr>
+            <tr>
+              <td style={{ width: "10%" , fontSize: '10px'}}>Island Trading</td>
+              <td style={{ width: "10%" , fontSize: '10px'}}>Helen Bennett</td>
+              <td style={{ width: "10%" , fontSize: '10px'}}>UK</td>
+            </tr>
+            <tr>
+              <td style={{ width: "10%" , fontSize: '10px'}}>Laughing Bacchus Winecellars</td>
+              <td style={{ width: "10%" , fontSize: '10px'}}>Yoshi Tannamuri</td>
+              <td style={{ width: "10%" , fontSize: '10px'}}>Canada</td>
+            </tr>
+            <tr>
+              <td style={{ width: "10%" , fontSize: '10px'}}>Magazzini Alimentari Riuniti</td>
+              <td style={{ width: "10%" , fontSize: '10px'}}>Giovanni Rovelli</td>
+              <td style={{ width: "10%" , fontSize: '10px'}}>Italy</td>
+            </tr>
+          </tbody>
         </table>
         <button onClick={generatePDF}>Download PDF</button>
       </div>
